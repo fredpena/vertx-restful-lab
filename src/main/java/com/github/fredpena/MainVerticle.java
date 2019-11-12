ec708c03943c672529fd239a68c27a79dc7a367b
Fred Pena <f.ant.pena@gmail.com>
2019-11-12 17:34:46 -0400

    - Init...

---
+++ src/main/java/com/github/fredpena/MainVerticle.java
@@ -0,0 +1,15 @@
+package com.github.fredpena;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fred Pena fantpena@gmail.com
+ */
+public class MainVerticle {
+
+}
